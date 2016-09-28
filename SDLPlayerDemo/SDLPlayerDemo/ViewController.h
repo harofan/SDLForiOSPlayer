470396b0b548a5198f648902d04e3477e28b507c
fy <fanyang_32012@outlook.com>
2016-09-28 09:38:50 +0800

    上传项目

---
+++ SDLPlayerDemo/SDLPlayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDLPlayerDemo
+//
+//  Created by fy on 2016/9/27.
+//  Copyright © 2016年 LY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SDLPlayerDemo/SDLPlayerDemo/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  SDLPlayerDemo
+//
+//  Created by fy on 2016/9/27.
+//  Copyright © 2016年 LY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#import "avformat.h"
+
+#import <SDL2/SDL.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    //打印ffmpeg配置信息
+//    NSLog(@"打印配置信息==================%s",avcodec_configuration());
+    
+    
+}
+
+@end
