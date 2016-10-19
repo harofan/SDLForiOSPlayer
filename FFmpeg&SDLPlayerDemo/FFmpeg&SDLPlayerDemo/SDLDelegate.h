d474ab027235dc63ce1f5101fea3345ed7ae8041
fy <fanyang_32012@outlook.com>
2016-10-19 09:51:30 +0800

    新模版重新建立工程

---
+++ FFmpeg&SDLPlayerDemo/FFmpeg&SDLPlayerDemo/SDLDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SDLDelegate.h
+//  sdltest1
+//
+//  Created by fy on 2016/10/18.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDLDelegate : UIResponder<UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
--- SDLPlayerDemo/SDLPlayerDemo/LXHSDLPlayerVC.m
+++ SDLPlayerDemo/SDLPlayerDemo/LXHSDLPlayerVC.m
@@ -19,6 +19,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    SDL_Init(SDL_INIT_VIDEO);
 }
 
 
